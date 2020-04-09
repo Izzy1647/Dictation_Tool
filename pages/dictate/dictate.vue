c53103858d58d9d3ab0b3dd24b90574ff189ce11
zzy <zhouhoushu@126.com>
2020-04-09 18:20:53 +0800

    dictation page

--- pages/dictate/dictate.vue
+++ pages/dictate/dictate.vue
@@ -1,6 +1,47 @@
 <template>
 	<view>
-		<cu-custom bgColor="bg-white" :isBack="true"><block slot="backText"></block><block slot="content">听写</block></cu-custom>
+        <cu-custom bgColor="bg-white" :isBack="true"><block slot="backText"></block><block slot="content">听写</block></cu-custom>
+		
+		<!-- 头部提示框 -->
+		<view class="cu-chat" style="position: relative; top: 10px;">
+			<view class="cu-item padding-top-xl">
+				<view class="cu-avatar radius" style="background-image:url(../../static/tabbar/about_cur.png);"></view>
+				<view class="main">
+					<view class="content shadow">
+						<text>听写中......答案写在答案区。</text>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+		
+		<!-- 用户输入框 -->
+		<view class="cu-form-group align-start">
+			<!-- <view class="title">答案区</view> -->
+			<textarea maxlength="-1" :disabled="modalName!=null" @input="textareaBInput" placeholder="写下你的答案吧"></textarea>
+		</view>
+		
+		<view class='cu-btn bg-red lg block shadow radius margin-xl' @tap="stopDictation" data-target="viewModal">
+			停止听写
+		</view>
+		
+		<view class='cu-btn bg-green lg block shadow radius margin-xl' @tap="showAnswer" data-target="viewModal">
+			查看答案
+		</view>
+		
+		<!-- 查看答案出现： -->
+		<view  v-if="ifAnswer">	
+			<view class="padding-sm">
+				<view class="flex flex-wrap justify-around">
+					<button class="margin-sm basis-sm shadow cu-btn" :class="['bg-' + item.color,toggleDelay?'animation-slide-bottom':'']"
+					 :style="[{animationDelay: (index + 1)*0.1 + 's'}]" v-for="(item,index) in list" :key="index">0.{{index+1}}s</button>
+				</view>
+			</view>
+			
+			<view class='cu-btn bg-green lg block shadow radius margin-xl' @tap="hideAnswer" data-target="viewModal">
+				隐藏答案
+			</view>
+		</view>
 		
 		
 		
@@ -29,8 +70,7 @@
 				<view :class="PageCur=='plugin'?'text-gray':'text-gray'">我的</view>
 			</view>
 		</view>
-		
-		
+
 	</view>
 </template>
 
@@ -39,9 +79,55 @@
 		data() {
 			return {
 				PageCur: 'words',
+				ifAnswer:false,     // true则显示答案
+				toggleDelay: false,
+				list: [{
+						name: 'fade',
+						color: 'red'
+					},
+					{
+						name: 'scale-up',
+						color: 'orange'
+					},
+					{
+						name: 'scale-down',
+						color: 'olive'
+					},
+					{
+						name: 'slide-top',
+						color: 'green'
+					}, {
+						name: 'slide-bottom',
+						color: 'cyan'
+					},
+					{
+						name: 'slide-left',
+						color: 'blue'
+					},
+					{
+						name: 'slide-right',
+						color: 'purple'
+					},
+					{
+						name: 'shake',
+						color: 'mauve'
+					}
+				]
 			}
 		},
 		methods: {
+			showAnswer(){
+				this.ifAnswer = true
+			},
+			
+			hideAnswer(){
+				this.ifAnswer = false
+			},
+			
+			stopDictation(){
+				console.log("Stop dictation...")
+			}
+		
 			
 		}
 	}
